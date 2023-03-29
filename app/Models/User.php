89ba0449eb0a57faf8d907e594ec2a37828bdf4b
M Abdulkarim <69695051+mabdulkarim@users.noreply.github.com>
2023-03-29 15:03:36 +0200

    Add exercise relationship in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Hash;
@@ -55,13 +56,18 @@ class User extends Authenticatable
         );
     }
 
-    public function userMeasurements()
+    public function userMeasurements(): HasMany
     {
         return $this->hasMany(UserMeasurement::class);
     }
 
-    public function workouts()
+    public function workouts(): HasMany
     {
         return $this->hasMany(Workout::class);
+    }
+
+    public function exercises(): HasMany
+    {
+        return $this->hasMany(Exercise::class);
     }
 }
