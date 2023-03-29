4c53ce95f6191823b41b794ad977813bfa6191fd
M Abdulkarim <69695051+mabdulkarim@users.noreply.github.com>
2023-03-29 13:12:50 +0200

    Create exercise factory and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
            UserSeeder::class,
            RolesPermissionsSeeder::class,
+           ExerciseSeeder::class,
         ]);
     }
 }
