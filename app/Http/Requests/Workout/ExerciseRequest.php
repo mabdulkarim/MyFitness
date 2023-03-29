91ce90ff935d9afb060ef19f6621682be454c76f
M Abdulkarim <69695051+mabdulkarim@users.noreply.github.com>
2023-03-29 15:05:12 +0200

    Create Exercise request

---
+++ app/Http/Requests/Workout/ExerciseRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests\Workout;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ExerciseRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => [
+                'required',
+                'max:64',
+                'string',
+            ],
+            'description' => [
+                'nullable',
+                'string',
+                'max:255',
+            ],
+        ];
+    }
+}
