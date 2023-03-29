3d8caf078ca86252f83cbf5b49b1bad5aba2b7dc
M Abdulkarim <69695051+mabdulkarim@users.noreply.github.com>
2023-03-29 15:16:50 +0200

    Refactor folder structure auth requests

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\LoginRequest;
+use App\Http\Requests\Auth\LoginRequest;
 use App\Http\Requests\Users\StoreUserRequest;
 use App\Models\User;
 use App\Models\UserMeasurement;
