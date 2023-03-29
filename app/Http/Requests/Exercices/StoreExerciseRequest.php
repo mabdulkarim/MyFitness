acb999c241ad143156f28a2ca45744df16b66bc8
M Abdulkarim <69695051+mabdulkarim@users.noreply.github.com>
2023-03-29 15:10:21 +0200

    Refactor folder structure exercise requests

--- app/Http/Requests/Exercices/StoreExerciseRequest.php
+++ app/Http/Requests/Exercices/StoreExerciseRequest.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Requests\Workout;
+namespace App\Http\Requests\Exercices;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class ExerciseRequest extends FormRequest
+class StoreExerciseRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
