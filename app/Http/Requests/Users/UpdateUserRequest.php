f3a4083d2db60cf91e9c46512f1fe00f754a0796
M Abdulkarim <69695051+mabdulkarim@users.noreply.github.com>
2023-03-29 15:17:31 +0200

    Refactor folder structure user requests

---
+++ app/Http/Requests/Users/UpdateUserRequest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Requests\Users;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => [
+                'string',
+                'max:255',
+            ],
+            'email' => [
+                'string',
+                'email',
+                'unique:users,email,'.auth()->id(),
+                'max:255',
+            ],
+            'password' => [
+                'string',
+                'min:8',
+                'confirmed',
+            ],
+            'age' => [
+                'numeric',
+                'between:16,120',
+            ],
+            'gender' => [
+                'string',
+                'in:male,female',
+            ],
+            'height' => [
+                'numeric',
+                'between:120,300',
+            ],
+        ];
+    }
+}
