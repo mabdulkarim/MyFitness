e1f014088597bec7aad0efbf904d93bbfa0df4aa
M Abdulkarim <69695051+mabdulkarim@users.noreply.github.com>
2023-03-29 14:59:39 +0200

    Create ExercisePolicy

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+use App\Models\Exercise;
 use App\Models\User;
+use App\Policies\ExercisePolicy;
 use App\Policies\UserPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
@@ -16,7 +18,8 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        User::class, UserPolicy::class,
+        User::class => UserPolicy::class,
+        Exercise::class => ExercisePolicy::class,
     ];
 
     /**
