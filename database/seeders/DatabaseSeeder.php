83228a3e9ea73bd8247c0f7001eb561d6cce5008
M Abdulkarim <69695051+mabdulkarim@users.noreply.github.com>
2023-03-29 12:08:29 +0200

    Clean database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,6 @@ class DatabaseSeeder extends Seeder
         $this->call([
            UserSeeder::class,
            RolesPermissionsSeeder::class,
-//           UserMeasurementSeeder::class
         ]);
     }
 }
